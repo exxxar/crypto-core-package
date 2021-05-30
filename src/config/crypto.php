3a5326a30a370de1c5693f14aa075f1b86aaab2b
unknown <exxxar@gmail.com>
2021-05-30 19:39:59 +0300

    добавил в мидлвар другой формат json

--- src/config/crypto.php
+++ src/config/crypto.php
@@ -4,4 +4,5 @@ return [
     "url"=>env("APP_CRYPTOGRAPHIC_URL","http://81.200.255.35:8080/cryptoservice"),
     'prefix' => 'crypto',
     'middleware' => ['web'],
+    'server_user_id' => env("SERVER_USER_ID"),
 ];
--- src/middleware/XApiVersionMiddleware.php
+++ src/middleware/XApiVersionMiddleware.php
@@ -10,20 +10,24 @@ class XApiVersionMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next, $version)
     {
         if (!$request->hasHeader("X-API-VERSION"))
-            return response()->json([
-               "message"=>"Need Api version"
+            return response()->json((object)[
+                "code" => 415,
+                "detail" => "Need Api version",
+                "title" => "Подключение к маршруту"
             ]);
 
-        if ($request->header("X-API-VERSION")!=$version)
-            return response()->json([
-                "message"=>"Bad Api version, required $version"
+        if ($request->header("X-API-VERSION") != $version)
+            return response()->json((object)[
+                "code" => 415,
+                "detail" => "Bad Api version, required $version",
+                "title" => "Подключение к маршруту"
             ]);
 
         return $next($request);
