63514892c1e9b8e30fc12598a58fb4b016517426
unknown <exxxar@gmail.com>
2021-06-03 20:36:16 +0300

    change service event@

--- src/listeners/HandleMSListener.php
+++ src/listeners/HandleMSListener.php
@@ -7,6 +7,7 @@ use Cryptolib\CryptoCore\Events\HandleMSEvent;
 use Cryptolib\CryptoCore\Events\HandlerResultFormEvent;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 
 class HandleMSListener
 {
@@ -28,13 +29,15 @@ class HandleMSListener
      */
     public function handle(HandleMSEvent $event)
     {
+        Log::info("test");
         if (is_null($event))
             return;
 
         $userPayloadService = new UserPayloadServiceForServer();
-
+        Log::info("test2");
         $hrf = $userPayloadService->handler($event->transferForm);
-
+        Log::info("test3");
         event(new HandlerResultFormEvent($hrf));
+        Log::info("test4");
     }
 }
